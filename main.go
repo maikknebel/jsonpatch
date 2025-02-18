0b8386dd2cafc96ae30da49516821788ac8dd163
Maik Knebel <maik.knebel@gmx.de>
2025-02-18 21:12:36 +0100

    build: add config for golangci-lint

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
+// The entrypoint for the package.
 package main
 
 func main() {
 	print("Hello World")
+
+	// size := 3
+	src := make([]int, 3)
+	// foo
+	dst := make([]int, 3)
+
+	for i, x := range src {
+		dst[i] = x
+	}
+	println(src)
 }
